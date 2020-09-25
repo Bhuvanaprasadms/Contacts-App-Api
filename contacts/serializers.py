2c15501d68a5862bfd4bdc430418138bbcec6a53
prasad <bhuvanaprasad.b.tech@gmail.com>
2020-09-25 19:06:50 +0530

    Added Applications Files

---
+++ contacts/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Contact
+
+
+class ContactSerializer(ModelSerializer):
+
+    class Meta:
+        model = Contact
+        fields = ['id','country_code','firstname','lastname','phone_number',
+        'contact_picture','is_favourited']
